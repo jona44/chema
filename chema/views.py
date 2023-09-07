81b5e2342c7658188585c671f7558ec61cf61356
jona44 <tjmanyadza@live.com>
2023-09-07 14:33:50 +0200

    master

---
+++ chema/views.py
@@ -0,0 +1,366 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import *
+from django.urls import reverse 
+from django.forms import inlineformset_factory
+from django.contrib import messages
+import os
+from user.models import Profile
+import random
+from .forms import *
+
+
+@login_required
+def home(request):
+    user = request.user
+    groups = Group.objects.filter(members=user)
+    search_form = SearchForm()
+    grouped_data = []
+    for group in groups:
+        posts = Post.objects.filter(group=group).order_by('-created_at')
+        comments = Comment.objects.filter(post__group=group).order_by('-created_at')
+        admins_as_members = group.admins_as_members.all()
+
+        group_data = {
+            'group': group,
+            'minimized': not (posts.exists() or comments.exists()),
+            'posts': posts[:3],  # Limit the number of posts to display initially
+            'comments': comments[:3],  # Limit the number of comments to display initially
+            'admins_as_members': admins_as_members,
+        }
+
+        for comment in comments:
+            comment.replies.set(Reply.objects.filter(comment=comment).order_by('-created_at')[:3])
+        grouped_data.append(group_data)
+
+    return render(request, 'chema/home.html', {'grouped_data': grouped_data, 'groups': groups,'search_form': search_form})
+
+
+
+
+@login_required
+def choice(request):
+    pass
+    return render(request, 'chema/choice.html')
+
+
+@login_required
+def join_existing_group(request):
+    if request.method == 'POST':
+        form = GroupJoinForm(request.POST)
+        if form.is_valid():
+            group_id = form.cleaned_data['group'].id
+            group = get_object_or_404(Group, id=group_id)
+            group.members.add(request.user)
+            return redirect('home')
+    else:
+        form = GroupJoinForm()
+
+    return render(request, 'chema/join_existing_group.html', {'form': form})
+
+
+@login_required
+def create_group(request):
+    if request.method == 'POST':
+        form = GroupForm(request.POST, request.FILES)
+        if form.is_valid():
+            group = form.save(commit=False)
+            group.admin = request.user  # Set the admin of the group to the current user
+            group.save()
+            group.members.add(request.user)  # Add the current user as a member
+            group.admins_as_members.add(request.user)  # Add the current user as an admin (if needed)
+            return redirect('home')
+    else:
+        form = GroupForm()
+    return render(request, 'chema/create_group.html', {'form': form})
+
+
+
+# def groupDetail(request, group_id):
+#     groups = get_object_or_404(Group, id=group_id)
+#     post = Post.objects.filter(group=groups)
+#     return render(request, 'chema/home.html', {'groups': groups,'post':post})
+
+
+from .models import Post
+from .forms import PostCreationForm
+
+@login_required
+def createPost(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+
+    if request.method == 'POST':
+        form = PostCreationForm(request.POST, request.FILES)  # Include request.FILES for handling image uploads
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.group = group
+            post.approved = False  # Set the post as unapproved by default
+            post.save()
+            return redirect('home')
+    else:
+        form = PostCreationForm()
+
+    return render(request, 'chema/createPost.html', {'form': form, 'group': group})
+
+
+@login_required
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == 'POST':
+        form = EditPostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Redirect to the home page after editing the post
+    else:
+        form = EditPostForm(instance=post)
+
+    return render(request, 'chema/edit_post.html', {'form': form, 'post': post})
+
+
+
+
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if post.author != request.user:
+        # If the current user is not the author of the post, they are not allowed to delete it
+        return redirect('home')
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')  # Redirect to home page or any other appropriate page after post deletion
+
+    return render(request, 'chema/delete_post.html', {'post': post})
+
+
+@login_required
+def create_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect('home')
+    else:
+        form = CommentForm()
+
+    return render(request, 'chema/create_comment.html', {'form': form, 'post': post})
+
+
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    if comment.author != request.user:
+        # If the current user is not the author of the comment, they are not allowed to edit it
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = CommentEditForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CommentEditForm(instance=comment)
+
+    return render(request, 'chema/edit_comment.html', {'form': form, 'comment': comment})
+
+
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    if comment.author != request.user:
+        # If the current user is not the author of the comment, they are not allowed to delete it
+        return redirect('post_detail', post_id=comment.post.id)
+
+    if request.method == 'POST':
+        comment.delete()
+        return redirect('post_detail', post_id=comment.post.id)
+
+    return render(request, 'chema/delete_comment.html', {'comment': comment})
+
+
+
+@login_required
+def add_member(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    user = request.user  # Get the logged-in user
+
+    if request.method == 'POST':
+        form = AddMemberForm(request.POST)
+        if form.is_valid():
+            new_member = form.cleaned_data['new_member']
+            # Check if the new_member is the logged-in user and not already a member
+            if new_member not in group.members.all():
+                group.members.add(new_member)
+                return redirect('home')
+    else:
+        form = AddMemberForm()
+
+    return render(request, 'chema/add_member.html', {'form': form, 'group': group})
+
+
+@login_required
+def approve_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+    # Check if the user is the group admin
+    if request.user == post.group.admin:
+        post.approved = True
+        post.save()
+    
+    return redirect('home')
+
+
+
+@login_required
+def add_dependents(request):
+    DependentFormSet = inlineformset_factory(User, Dependent, form=DependentForm, extra=3)  # Set the number of empty forms
+
+    user = request.user
+    formset = DependentFormSet(instance=user)
+
+    if request.method == 'POST':
+        formset = DependentFormSet(request.POST, instance=user)
+        if formset.is_valid():
+            formset.save()
+            
+             # Getting the names of the added dependents
+            added_dependents_names = ', '.join([form.cleaned_data.get('name') for form in formset.forms if form.cleaned_data.get('name')])
+
+            # Adding a success message with the names of the added dependents
+            success_message = f"Successfully added dependents: {added_dependents_names}"
+            messages.success(request, success_message)
+            return redirect('home')  # Redirect to the user's home page
+
+    return render(request, 'chema/add_dependents.html', {'formset': formset})
+
+
+@login_required
+def user_groups(request):
+    user = request.user
+    groups = Group.objects.filter(members=user)
+    return render(request, 'chema/user_groups.html', {'groups': groups})
+
+
+@login_required
+def add_reply(request, comment_id):
+    comment= get_object_or_404(Comment, id=comment_id)
+
+    if request.method == 'POST':
+        form = ReplyForm(request.POST)
+        if form.is_valid():
+            new_reply = form.save(commit=False)
+            new_reply.author = request.user
+            new_reply.comment = comment
+            new_reply.save()
+            return redirect('home')
+    else:
+        form = ReplyForm()
+
+    return render(request, 'chema/add_reply.html', {'form': form, 'comment': comment})
+
+
+@login_required
+def remove_reply(request, reply_id):
+    reply = get_object_or_404(Reply, id=reply_id)
+    comment = reply.comment
+    if request.user == reply.author:
+        reply.delete()
+    return redirect('groupDetail', group_id=comment.post.group.id)
+
+
+@login_required
+def edit_reply(request, reply_id):
+    reply = get_object_or_404(Reply, id=reply_id)
+
+    if request.method == 'POST':
+        form = EditReplyForm(request.POST, instance=reply)
+        if form.is_valid():
+            form.save()
+            return redirect('groupDetail', group_id=reply.comment.post.group.id)
+    else:
+        form = EditReplyForm(instance=reply)
+
+    return render(request, 'chema/edit_reply.html', {'form': form, 'reply': reply})
+
+
+
+@login_required
+def member_detail(request, pk):
+    # Get the member object or raise a 404 error
+    member = get_object_or_404(User, pk=pk)
+    # Get the member's profile
+    profile = Profile.objects.get(user=member)
+    # Get the groups that the member belongs to
+    groups = Group.objects.filter(members=member)
+    # Get the dependents of the member
+    dependents = Dependent.objects.filter(guardian=member)
+    # Get the profile image of the member
+    
+    # Render the template with the context data
+    return render(request, 'chema/member_detail.html', {
+        'member': member,
+        'profile': profile,
+        'groups': groups,
+        'dependents': dependents,
+        
+    })
+
+
+def search_view(request):
+    query = request.GET.get('q', '')
+    # Perform your search query here
+    results = list(Group.objects.filter(name__icontains=query).values())
+    return JsonResponse({'results': results})
+
+
+def join_group(request, group_id):
+    # Get the group object
+    group = get_object_or_404(Group, id=group_id)
+    # Add the current user to the group's members
+    if request.user not in group.members.all():
+        group.members.add(request.user)
+    # Redirect to the group's detail page or any other appropriate page
+    return redirect('group_detail_view', group_id)
+
+
+def group_detail_view(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    members_count = group.members.count()
+    admins_count = group.admins_as_members.count()
+    
+    return render(request, 'chema/group_detail_view.html', {'group': group,'members_count':members_count,'admins_count':admins_count})
+
+
+
+@login_required
+def add_admin(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    if request.method == 'POST':
+        form = AddAdminForm(request.POST, group_id=group.id)
+        if form.is_valid():
+            user = form.cleaned_data['member']
+            if request.user in group.admins_as_members.all() and user in group.members.all():
+                group.admins_as_members.add(user)
+                return redirect('group_detail_view', group_id=group.id)
+            else:
+                # handle error case here
+                pass
+    else:
+        form = AddAdminForm(group_id=group.id)
+    return render(request, 'chema/add_admin.html', {'form': form})
+
+
+
+
