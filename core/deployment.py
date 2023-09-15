279c9390d970060549d2e8374bf8b1977d6e76cd
jona44 <tjmanyadza@live.com>
2023-09-15 14:36:27 +0200

    req django

--- core/deployment.py
+++ core/deployment.py
@@ -3,7 +3,7 @@ from .settings import *
 from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOST = ['chemaonline.azurewebsites.net']
+ALLOWED_HOST = ['WEBSITE_HOSTNAME']
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 EMAIL_HOST_USER = os.environ['EMAIL_PASSWORD']
 
