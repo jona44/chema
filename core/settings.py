3f70358cb4ebbee716b54d92c4a1bc1fe3d0cce2
jona44 <tjmanyadza@live.com>
2023-10-26 20:02:48 +0200

    'allmore'

--- core/settings.py
+++ core/settings.py
@@ -88,7 +88,7 @@ AUTHENTICATION_BACKENDS = (
     # ...
 )
 
-DATABASES = {
+DATABASES ={
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
