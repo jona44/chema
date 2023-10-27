d2a1a69dbcf3263fcbf2189324ba531c097370cc
jona44 <tjmanyadza@live.com>
2023-10-27 12:20:30 +0200

    'all'

--- core/deployment.py
+++ core/deployment.py
@@ -22,9 +22,9 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
-# parameters = {pair.split('=')(0):pair.split('=')(1) for pair in connection_string.split('')}
-parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(';')}
+connection_string = os.environ('AZURE_POSTGRESQL_CONNECTIONSTRING')
+parameters = {pair.split('=')(0):pair.split('=')(1) for pair in connection_string.split('')}
+# parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(';')}
 
 
 DATABASES = {
