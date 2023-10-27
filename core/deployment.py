da2ea6e6f4378dfb5ec0e470b89b7ef349eb24bc
jona44 <tjmanyadza@live.com>
2023-10-27 10:15:56 +0200

    'all'

--- core/deployment.py
+++ core/deployment.py
@@ -5,7 +5,6 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOST = ['WEBSITE_HOSTNAME']
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
-EMAIL_HOST_USER = os.environ['EMAIL_PASSWORD']
 
 DEBUG = False
 
@@ -35,3 +34,11 @@ DATABASES = {
         'PASSWORD':parameters['password'],
     }    
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER ='manyadzatocky@gmail.com'
+EMAIL_HOST_PASSWORD  = os.environ['EMAIL_PASSWORD']
+ 
