96a9f3b22d3774ea29ac4e629cecf1537047dc52
jona44 <tjmanyadza@live.com>
2023-10-27 23:44:39 +0200

    'all'

--- core/deployment.py
+++ core/deployment.py
@@ -30,7 +30,8 @@ STORAGES = {
 }
 
 # connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-connection_string = os.environ.get['AZURE_POSTGRESQL_CONNECTIONSTRING']
+# connection_string = os.environ.get['AZURE_POSTGRESQL_CONNECTIONSTRING']
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 # parameters = {pair.split('='):pair.split('=')(1) for pair in connection_string.split(' ')}
 parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(';')}
 
