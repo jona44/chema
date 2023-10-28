4ff6476e0084783355acb98499bdd90bf3900d50
jona44 <tjmanyadza@live.com>
2023-10-28 22:04:17 +0200

    'all'

--- core/deployment.py
+++ core/deployment.py
@@ -2,8 +2,8 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-from azure.identity import DefaultAzureCredential
-import psycopg2
+# from azure.identity import DefaultAzureCredential
+# import psycopg2
 
 
 SECRET_KEY = os.environ['SECRET']
@@ -23,8 +23,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 STORAGES = {
     # ...
@@ -36,7 +36,7 @@ STORAGES = {
 
 # Uncomment the following lines according to the authentication type.
 # For system-assigned identity.
-credential = DefaultAzureCredential()
+#credential = DefaultAzureCredential()
 
 # For user-assigned identity.
 # managed_identity_client_id = os.getenv('AZURE_POSTGRESQL_CLIENTID')
@@ -49,49 +49,47 @@ credential = DefaultAzureCredential()
 # cred = ClientSecretCredential(tenant_id=tenant_id, client_id=client_id, client_secret=client_secret)
 
 # Acquire the access token.
-accessToken =credential.get_token('https://ossrdbms-aad.database.windows.net/.default')
+#accessToken =credential.get_token('https://ossrdbms-aad.database.windows.net/.default')
 
 
-# In your setting file, eg. settings.py
-host = os.getenv('AZURE_POSTGRESQL_HOST')
-user = os.getenv('AZURE_POSTGRESQL_USER')
-password = accessToken.token # this is accessToken acquired from above step.
-database = os.getenv('AZURE_POSTGRESQL_NAME')
+# # In your setting file, eg. settings.py
+# host = os.getenv('AZURE_POSTGRESQL_HOST')
+# user = os.getenv('AZURE_POSTGRESQL_USER')
+# password = accessToken.token # this is accessToken acquired from above step.
+# database = os.getenv('AZURE_POSTGRESQL_NAME')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'AZURE_POSTGRESQL_NAME',
-        'USER': 'AZURE_POSTGRESQL_USER',
-        'PASSWORD': 'accessToken',
-        'HOST': 'AZURE_POSTGRESQL_HOST',
-        'PORT': '5432',  # Port is 5432 by default 
-        'OPTIONS': {'sslmode': 'require'},
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'AZURE_POSTGRESQL_NAME',
+#         'USER': 'AZURE_POSTGRESQL_USER',
+#         'PASSWORD': 'accessToken',
+#         'HOST': 'AZURE_POSTGRESQL_HOST',
+#         'PORT': '5432',  # Port is 5432 by default 
+#         'OPTIONS': {'sslmode': 'require'},
+#     }
+# }
 
 
 
-# connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 # connection_string = os.environ.get['AZURE_POSTGRESQL_CONNECTIONSTRING']
 #connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-# parameters = {pair.split('='):pair.split('=')(1) for pair in connection_string.split(' ')}
+parameters = {pair.split('='):pair.split('=')(1) for pair in connection_string.split(' ')}
 #parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(';')}
 
 
 #learn.microsoft.com/en-us/azure/service-connector/how-to-integrate-postgres
 
-# DATABASES = {
-#     'default':{
-#         'ENGINE':'django.db.backends.postgresql',
-#         'NAME':parameters["dbname"],
-#         'HOST':parameters['host'],
-#         'USER':parameters['user'],
-#         'PORT':parameters[5432] ,
-#         'SSLMODE':parameters['require'],
-#         'PASSWORD':parameters['password'],
-#     }    
-# }
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':parameters["dbname"],
+        'HOST':parameters['host'],
+        'USER':parameters['user'],
+        'PASSWORD':parameters['password'],
+    }    
+}
 # DATABASES = {
 #     'default':{
 #         'ENGINE':'django.db.backends.postgresql',
@@ -111,6 +109,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER ='manyadzatocky@gmail.com'
 EMAIL_HOST_PASSWORD  = os.environ['EMAIL_PASSWORD']
 
-WEBSITES_PORT = 8000
-WEBSITES_CONTAINER_START_TIME_LIMIT = 1800
+
  
