bdc6ab9434b9d96ace4c32d06f1ab7c478310fd6
jona44 <tjmanyadza@live.com>
2023-10-28 23:49:11 +0200

    'all'

--- core/deployment.py
+++ core/deployment.py
@@ -77,7 +77,7 @@ connection_string = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
 
 #connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 #parameters = {pair.split('='):pair.split('=')(1) for pair in connection_string.split(' ')}
-parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(';')}
+parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
 
 
 #learn.microsoft.com/en-us/azure/service-connector/how-to-integrate-postgres
