7fa4ade7e5dc4b30a141273e53e7e7ed1534184e
jona44 <tjmanyadza@live.com>
2023-10-29 21:06:10 +0200

    'all'

--- staticfiles/condolence/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create-contribution/', views.create_contribution, name='create_contribution'),
-    path('contribution/<int:contribution_id>/', views.contribution_detail, name='contribution_detail'), 
-    path('contributions_list/', views.contributions_list, name='contributions_list'),
-]
-
