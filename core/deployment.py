8cfce16660db74fb596a3602339de04d92c1ba57
tjmanyadza@live.com <tjmanyadza@live.com>
2023-10-31 20:44:16 +0200

    'deplo

--- core/deployment.py
+++ core/deployment.py
@@ -2,8 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-# from azure.identity import DefaultAzureCredential
-# import psycopg2
+
 
 
 SECRET_KEY = os.environ['SECRET']
@@ -34,49 +33,14 @@ STORAGES = {
 }
 
 
-# Uncomment the following lines according to the authentication type.
-# For system-assigned identity.
-#credential = DefaultAzureCredential()
-
-# For user-assigned identity.
-# managed_identity_client_id = os.getenv('AZURE_POSTGRESQL_CLIENTID')
-# cred = ManagedIdentityCredential(client_id=managed_identity_client_id)    
-
-# For service principal.
-# tenant_id = os.getenv('AZURE_POSTGRESQL_TENANTID')
-# client_id = os.getenv('AZURE_POSTGRESQL_CLIENTID')
-# client_secret = os.getenv('AZURE_POSTGRESQL_CLIENTSECRET')
-# cred = ClientSecretCredential(tenant_id=tenant_id, client_id=client_id, client_secret=client_secret)
-
-# Acquire the access token.
-#accessToken =credential.get_token('https://ossrdbms-aad.database.windows.net/.default')
-
-
-# In your setting file, eg. settings.py
-# host = os.getenv('AZURE_POSTGRESQL_HOST')
-# user = os.getenv('AZURE_POSTGRESQL_USER')
-# password = accessToken.token # this is accessToken acquired from above step.
-# database = os.getenv('AZURE_POSTGRESQL_NAME')
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'AZURE_POSTGRESQL_NAME',
-#         'USER': 'AZURE_POSTGRESQL_USER',
-#         'PASSWORD': 'accessToken',
-#         'HOST': 'AZURE_POSTGRESQL_HOST',
-#         'PORT': '5432',  # Port is 5432 by default 
-#         'OPTIONS': {'sslmode': 'require'},
-#     }
-# }
 
 
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-# connection_string = os.environ.get['AZURE_POSTGRESQL_CONNECTIONSTRING']
+connection_string = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
+
 #connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
-#parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(';')}
+#parameters = {pair.split('='):pair.split('=')(1) for pair in connection_string.split(' ')}
+parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
 
 
 #learn.microsoft.com/en-us/azure/service-connector/how-to-integrate-postgres
@@ -87,20 +51,10 @@ DATABASES = {
         'NAME':parameters["dbname"],
         'HOST':parameters['host'],
         'USER':parameters['user'],
-         'PASSWORD':parameters['password'],
+        'PASSWORD':parameters['password'],
     }    
 }
-# DATABASES = {
-#     'default':{
-#         'ENGINE':'django.db.backends.postgresql',
-#         'NAME':'chemaonline-database ',
-#         'HOST':'chemaonline-server.postgres.database.azure.com',
-#         'USER':'labcfxiqdd',
-#         'PORT': 5432,
-#         'SSLMODE':'require',
-#         'PASSWORD':'Y1FZAVFH4X38SN7J$'
-#     }    
-# }
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
