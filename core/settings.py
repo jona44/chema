5778cdde3c791c67d3a1441c857f1787e428c999
tjmanyadza@live.com <tjmanyadza@live.com>
2023-11-21 18:48:37 +0200

    all

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['chemaonline.azurewebsites.net','127.0.0.1','chema.com']
 CSRF_TRUSTED_ORIGINS = ['https://chemaonline.azurewebsites.net','https://127.0.0.1','https://chema.com']
 
-SITE_ID = 10
+SITE_ID = 11
 
 
 INSTALLED_APPS = [
