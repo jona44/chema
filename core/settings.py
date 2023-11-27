020ad520d9e16658571b1325ef967276dbf90e1d
tjmanyadza@live.com <tjmanyadza@live.com>
2023-11-27 17:42:36 +0200

    staticfiles

--- core/settings.py
+++ core/settings.py
@@ -107,20 +107,17 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+
 
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'static/'
- 
-STATICFILES_DIRS = [BASE_DIR, 'static']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-    
-    
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
