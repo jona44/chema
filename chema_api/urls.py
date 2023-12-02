e0ec52e285e4e10895c56d967ec69aaebb041aaf
tjmanyadza@live.com <tjmanyadza@live.com>
2023-12-02 22:53:26 +0200

    API

--- chema_api/urls.py
+++ chema_api/urls.py
@@ -15,6 +15,13 @@ urlpatterns = [
     path('api/delete_comment/<int:comment_id>/', views.delete_comment_api, name='delete_comment_api'),
     path('api/add_member/<int:group_id>/', views.add_member_api, name='add_member_api'),
     path('api/add_dependent/', views.add_dependent_api, name='add_dependent_api'),
+    path('api/add_reply/<int:comment_id>/', views.add_reply_api, name='add_reply_api'),
+    path('api/remove_reply/<int:reply_id>/', views.remove_reply_api, name='remove_reply_api'),
+    path('api/edit_reply/<int:reply_id>/', views.edit_reply_api, name='edit_reply_api'),
+    path('api/search/', views.search_api, name='search_api'),
+    path('api/member_detail/<int:group_id>/<int:member_id>/', views.member_detail_api, name='member_detail_api'),
+    path('api/add_admin/<int:group_id>/', views.add_admin_api, name='add_admin_api'),
+    path('api/toggle_group/<int:group_id>/', views.toggle_group_api, name='toggle_group_api'),
     
     # Add other API endpoints as needed
 ]
--- chema_api/views.py
+++ chema_api/views.py
@@ -1,11 +1,15 @@
 # views.py in your existing app
 
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from chema.models import Group
+from chema.models import *
 from .serializers import *
+from condolence.models import Profile, Group, Deceased, Contribution
+from .serializers import ProfileSerializer, GroupSerializer, DeceasedSerializer, ContributionSerializer
+
 
 
 @api_view(['GET'])
@@ -249,4 +253,196 @@ def add_dependent_api(request):
    
  
     
-    
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_reply_api(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+
+    if request.method == 'POST':
+        # Process the form submission
+        serializer = ReplySerializer(data=request.data)
+        if serializer.is_valid():
+            # Set the author and comment based on the request
+            serializer.validated_data['author'] = request.user.profile
+            serializer.validated_data['comment'] = comment
+
+            # Save the reply
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# views.py in your existing app
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def remove_reply_api(request, reply_id):
+    reply = Reply.objects.get(id=reply_id)
+
+    if request.user.profile == reply.author:
+        reply.delete()
+        return Response({"detail": "Reply deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+    return Response({"detail": "You don't have permission to delete this reply."}, status=status.HTTP_403_FORBIDDEN)
+
+
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def edit_reply_api(request, reply_id):
+    reply = Reply.objects.get(id=reply_id)
+
+    if request.user.profile == reply.author:
+        if request.method == 'PUT':
+            # Process the form submission
+            serializer = ReplySerializer(reply, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response({"detail": "You don't have permission to edit this reply."}, status=status.HTTP_403_FORBIDDEN)
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def group_detail_api(request, group_id):
+    group = Group.objects.get(pk=group_id)
+    deceased = Deceased.objects.filter(group__is_active=True, group=group).order_by('-date')
+
+    # Use the reverse relationship to get the admin members
+    group_admins = group.members.filter(groupmembership__is_admin=True)
+
+    # Serialize the data
+    group_serializer = GroupSerializer(group)
+    deceased_serializer = DeceasedSerializer(deceased, many=True)
+
+    # Construct the response data
+    response_data = {
+        'group': group_serializer.data,
+        'group_admins': group_admins.values_list('id', flat=True),  # Assuming you want to include only the IDs of admins
+        'members': group.members.values_list('id', flat=True),  # Assuming you want to include only the IDs of members
+        'count_members': group.members.count(),
+        'count_admins': group_admins.count(),
+        'deceased': deceased_serializer.data,
+    }
+
+    return Response(response_data)
+
+
+# views.py in your existing app
+
+
+from .serializers import GroupSerializer, ProfileSerializer
+from django.db.models import Q
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def search_api(request):
+    query = request.GET.get('q', '')
+
+    # Perform a search for both groups and members using Q objects
+    groups = Group.objects.filter(Q(name__icontains=query) | Q(members__user__username__icontains=query))
+    profiles = Profile.objects.filter(user__username__icontains=query)
+
+    # Serialize the data
+    group_serializer = GroupSerializer(groups, many=True)
+    profile_serializer = ProfileSerializer(profiles, many=True)
+
+    # Construct the response data
+    response_data = {
+        'groups': group_serializer.data,
+        'profiles': profile_serializer.data,
+    }
+
+    return Response(response_data)
+# views.py in your existing app
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def member_detail_api(request, group_id, member_id):
+    member = Profile.objects.get(id=member_id)
+    groups = member.groups.all()
+    deceased = Deceased.objects.filter(deceased_id=member_id, group_id=group_id)
+    contribution = Contribution.objects.filter(contributing_member=member_id, group__is_active=True)
+
+    # Serialize the data
+    member_serializer = ProfileSerializer(member)
+    group_serializer = GroupSerializer(groups, many=True)
+    deceased_serializer = DeceasedSerializer(deceased, many=True)
+    contribution_serializer = ContributionSerializer(contribution, many=True)
+
+    # Construct the response data
+    response_data = {
+        'member': member_serializer.data,
+        'groups': group_serializer.data,
+        'deceased': deceased_serializer.data,
+        'contribution': contribution_serializer.data,
+    }
+
+    return Response(response_data)
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_admin_api(request, group_id):
+    group = Group.objects.get(id=group_id)
+
+    # Check if the user is an admin of the group
+    group_admins = group.get_admins()
+
+    # Check if the current user is not in the list of admins
+    if request.user.profile not in [admin.user.profile for admin in group_admins]:
+        return Response({'detail': 'You do not have permission to add an admin to this group.'}, status=403)
+
+    # Process the form submission
+    serializer = GroupMembershipSerializer(data=request.data)
+    if serializer.is_valid():
+        user_id = serializer.validated_data['member']
+
+        # Check if the selected user is a member of the group
+        if group.members.filter(id=user_id).exists():
+            # Check if the user is already an admin
+            if not group.groupmembership_set.filter(member__id=user_id, is_admin=True).exists():
+                # If not an admin, make the user an admin
+                group_membership = group.groupmembership_set.get(member__id=user_id)
+                group_membership.is_admin = True
+                group_membership.save()
+
+                return Response({'detail': 'User added as an admin successfully.'}, status=200)
+            else:
+                return Response({'detail': 'User is already an admin of the group.'}, status=400)
+        else:
+            return Response({'detail': 'Selected user is not a member of the group.'}, status=400)
+
+    return Response(serializer.errors, status=400)
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def toggle_group_api(request, group_id):
+    # Get the group being toggled
+    group_to_toggle = get_object_or_404(Group, id=group_id)
+
+    # Toggle the group by setting it to active
+    group_to_toggle.is_active = not group_to_toggle.is_active
+    group_to_toggle.save()
+
+    # Deactivate all other groups
+    Group.objects.exclude(id=group_id).update(is_active=False)
+
+    # Serialize the updated group
+    group_serializer = GroupSerializer(group_to_toggle)
+
+    # Construct the response data
+    response_data = {
+        'detail': 'Group toggled successfully.',
+        'group': group_serializer.data,
+    }
+
+    return Response(response_data)
+
