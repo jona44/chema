cf42ac676bfd0e348eaeea7fe98a12b2f285dfb2
tjmanyadza@live.com <tjmanyadza@live.com>
2023-12-04 23:30:59 +0200

    api

--- condolence_api/urls.py
+++ condolence_api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .import views 
 
 urlpatterns = [
-     path('create_contribution_api/', views.create_contribution_api, name='create_contribution'),
+     path('create_contribution_api/', views.create_contribution_api, name='create_contribution_api'),
      path('contribution_detail_api/<int:contribution_id>/', views.contribution_detail_api, name='contribution_detail_api'),
-     path('deceased_api/', views.deceased_api, name='deceased'),
+     path('deceased_api/', views.deceased_api, name='deceased_api'),
      path('toggle_deceased_api/<int:deceased_d>/', views.toggle_deceased_api, name='toggle_deceased_api'),
      path('stop_contributions_api/<int:deceased_d>/', views.stop_contributions_api, name='stop_contributions_api'),
 
